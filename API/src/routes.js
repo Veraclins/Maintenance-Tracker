facd91dbcf9611599b46e0bf944cdac17ff79e74
Agada Clinton <agadaclinton@gmail.com>
2018-05-19 23:32:19 +0100

    Add code for post to /v1/users and test (passing)
    Added the codes necessary for the tests in post to /users to pass that
    is: create a user with the required paramaters, save it to memory and
    return it

--- API/src/routes.js
+++ API/src/routes.js
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-
+import users from './storage/users';
 
 const routes = Router();
 
@@ -8,5 +8,21 @@ routes.get('/', (req, res) => {
   res.send({ message: 'Welcome to Maintenance Tracker API.' });
 });
 
+routes.post('/users', (req, res) => {
+  const token = Math.random().toString(36).substring(2, 15)
+  + Math.random().toString(36).substring(2, 15);
+  const user = {
+    id: 1,
+    name: 'Agada Clinton',
+    email: 'agadaclinton@gmail.com',
+    password: 'password',
+    user_code: 'AD001',
+    dept: 'Web-development',
+    role: 'admin',
+    token,
+  };
+  users.push(user);
+  res.send(user);
+});
 
 export default routes;
