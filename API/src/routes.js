2aa6a38be6dd12940144cf74e3bedfcc932d618b
Agada Clinton <agadaclinton@gmail.com>
2018-05-19 23:16:45 +0100

    Add root route and test for user signup
    Added the codes for the root route and test it (passed) and add the
    test for user sign-up and expect the status code to be ok and for the it
    to create the user and return it.

--- API/src/routes.js
+++ API/src/routes.js
@@ -1,8 +1,12 @@
 import { Router } from 'express';
 
+
 const routes = Router();
 
 // Routes
+routes.get('/', (req, res) => {
+  res.send({ message: 'Welcome to Maintenance Tracker API.' });
+});
 
 
 export default routes;
--- API/test/index.spec.js
+++ API/test/index.spec.js
@@ -1,6 +1,7 @@
 import chai from 'chai';
 import chaiHttp from 'chai-http';
 import { server } from '../src/index';
+import users from '../src/storage/users';
 
 const expect = chai.expect; // eslint-disable-line prefer-destructuring
 chai.use(chaiHttp);
@@ -20,6 +21,41 @@ describe('Root route, /v1/', () => {
       .get('/v1/')
       .end((err, res) => {
         expect(res.body).to.have.property('message');
+        done();
+      });
+  });
+});
+
+describe('POST to /v1/users', () => {
+  const user = {
+    id: 1,
+    name: 'Agada Clinton',
+    email: 'agadaclinton@gmail.com',
+    password: 'password',
+    user_code: 'AD001',
+    dept: 'Web-development',
+    role: 'admin',
+    token: '',
+  };
+  it('responds with status 200', (done) => {
+    chai.request(server)
+      .post('/v1/users')
+      .end((err, res) => {
+        expect(res).to.have.status(200);
+        done();
+      });
+  });
+
+  it('it should create a user and return it', (done) => {
+    chai.request(server)
+      .post('/v1/users')
+      .send({ user })
+      .end((err, res) => {
+        expect(users).to.be.an('array').that.is.not.empty; // eslint-disable-line no-unused-expressions
+        expect(res.body).to.be.an('object');
+        expect(res.body).to.have.property('name');
+        expect(res.body).to.have.property('email');
+        expect(res.body).to.have.property('password');
         done();
       });
   });
